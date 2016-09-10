49e51c34a0461a50a48e19efd353d42f824fc865
OakG <oak.garcia@gmail.com>
2016-09-10 07:11:38 -0300

    3rd first try

---
+++ chessboard.js
@@ -0,0 +1,30 @@
+/* Write a program that creates a string that represents an 8×8 grid, using
+newline characters to separate lines. At each position of the grid there is
+either a space or a “#” character. The characters should form a chess board.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+*/
+
+board = "";
+size = 8;
+for (var n = 1; n <= size; n++) {
+    for (var i = 1; i <= size; i++) {
+        if (i%2 !== 0) {
+            board = board + " ";
+        } else {
+            board = board + "#";
+        }
+        if (i === size) {
+            board = board + "\n";
+        }
+    }
+}
