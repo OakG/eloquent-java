89c94f5c6bb660e0e6b0517c1fe219066f25fec3
OakG <oak.garcia@gmail.com>
2016-09-10 09:19:24 -0300

    Functions

--- chapter03/functions00.js
+++ chapter03/functions00.js
@@ -5,3 +5,19 @@ var square = function(x) { // Quantos parametros forem necessarios.
 };                         //Não esquecer o ponto e vírgula.
 
 console.log(square(5));
+
+var makeNoise = function() {
+    console.log("Pling!");
+};
+
+makeNoise();
+
+var power = function(base, exponent) {
+    var result = 1;
+    for (var count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
+};
+
+console.log(power(2, 10));
