d1c0e7f353f40417f81f4ac03ef74cf38e408e20
OakG <oak.garcia@gmail.com>
2016-09-10 09:17:07 -0300

    Functions

---
+++ chapter03/functions00.js
@@ -0,0 +1,7 @@
+//Defining a square function
+
+var square = function(x) { // Quantos parametros forem necessarios.
+    return x * x;
+};                         //Não esquecer o ponto e vírgula.
+
+console.log(square(5));
