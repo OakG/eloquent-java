15371c2434840b51d49ec4cea4f23fbb4045153e
OakG <oak.garcia@gmail.com>
2016-09-11 01:29:22 -0300

    minimun

---
+++ minimun.js
@@ -0,0 +1,10 @@
+var min = function(num1, num2) {
+    if (num1 > num2 ) {
+        return num2;
+    } else {
+            return num1;
+    }
+};
+console.log(min(2,11));
+console.log(min(12,7));
+console.log(min(-200,-100));
