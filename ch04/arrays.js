57c5a0828b4d4a7d5cf7ddb16a1c05d5246fe8fb
OakG <oak.garcia@gmail.com>
2016-09-13 01:10:53 -0300

    fiddling

--- ch04/arrays.js
+++ ch04/arrays.js
@@ -1,8 +1,12 @@
 
 var listOfNumbers = [2, 3, 5, 7, 11];
 
-console.log(listOfNumbers[1]);
+console.log(listOfNumbers[1]); // returns 3.
 
-// listOfNumbers[i] summons property of i = index, 0, 1, 23, etc.
+console.log(listOfNumbers[-1]); // returns undefined.
 
-console.log(listOfNumbers[-1]);
+console.log(listOfNumbers[1.5]); // also undefined.
+
+// listOfNumbers[i];
+//   i = 0 first
+// i is always 0 or a positive integer.
